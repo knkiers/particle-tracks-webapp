89df3f26244b6c303892131eae4705716b7ef7dc
Ken Kiers <knkiers@taylor.edu>
2017-10-28 10:57:56 -0400

    add in some checks of the student data

--- src/app/admin/event-energy-momentum/event-energy-momentum.component.ts
+++ src/app/admin/event-energy-momentum/event-energy-momentum.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import {EventDisplayService} from '../../shared/services/event-display.service';
 
@@ -15,18 +15,26 @@ import {EventDisplayService} from '../../shared/services/event-display.service';
 })
 export class EventEnergyMomentumComponent implements OnInit {
 
-  eventData: any = null;
-  eventActivatedDots: any = null;
+  @Input() eventData: any = null;
+  eventActivatedDots: any = null; // an array containing sets of activated dots and other properties for each particle in the event
   studentData: any = null;
   eventDataSummary: any = null;
+  errorMessages: any = [];
+  studentDataIsValid: boolean = true;
+  numberChargedParticles: number = 0;
+  numberNeutralParticles: number = 0;
+  incomingIsCharged: boolean = true;
+  neutralParticleData: any = null; // if there is a neutral particle (assumed to at most one neutral particle!), this will contain some of its data
 
   constructor(private eventDisplayService: EventDisplayService) { }
 
   ngOnInit() {
     //console.log('eventData: ', this.eventData);
     this.buildEventDataSummary();
+    this.characterizeEvent(); // determine # of charged/neutral particles, etc.
     this.determineActivatedDots();
     this.buildStudentData();
+    this.checkErrors();
 
   }
 
@@ -65,6 +73,25 @@ export class EventEnergyMomentumComponent implements OnInit {
       });
     });
   }
+
+  characterizeEvent() {
+    this.incomingIsCharged = !(this.eventData.event.parent.charge === 0);
+    this.numberChargedParticles = 0;
+    this.numberNeutralParticles = 0;
+    if (this.incomingIsCharged) {
+      this.numberChargedParticles++;
+    } else {
+      this.numberNeutralParticles++;
+    }
+    this.eventData.event.decay_products.forEach(particle => {
+      if (particle.charge === 0) {
+        this.numberNeutralParticles++;
+      } else {
+        this.numberChargedParticles++;
+      }
+    });
+  }
+
   // this method is used to determine what the students should get for the
   // various momenta, based on the radii of the circles, etc.
   buildStudentData() {
@@ -85,7 +112,9 @@ export class EventEnergyMomentumComponent implements OnInit {
       this.studentData.push(
         {
           circleNumber: circleNumber,
-          inout: circle.incoming ? 'in' : 'out',
+          inout: circle.incoming ? 'in' : 'out',//text for template
+          incoming: circle.incoming,//boolean
+          CW: circle.CW,
           r: circle.r,
           pMag: pMag,
           px: pMag*Math.cos(circle.theta),
@@ -94,7 +123,8 @@ export class EventEnergyMomentumComponent implements OnInit {
           mass: bestFitMass,
           energy: energyFit,
           name: name,
-          activatedDotsIndex: bestFitIndex
+          activatedDotsIndex: bestFitIndex,
+          error: false
         }
       );
       circleNumber++;
@@ -123,7 +153,9 @@ export class EventEnergyMomentumComponent implements OnInit {
         dotIndices: pathParams.activatedDots,
         mass: this.eventData.event.parent.mass,
         index: activatedDotIndex,
-        name: this.eventData.event.parent.particle_name
+        name: this.eventData.event.parent.particle_name,
+        incoming: true,
+        CW: particleDirection === 'cw'
       });
       activatedDotIndex++;
     }
@@ -137,15 +169,24 @@ export class EventEnergyMomentumComponent implements OnInit {
         let particleDirection = this.eventDisplayService.inOut(bFieldDirection, charge);
         let pathParams = this.eventDisplayService.curvedPathParams(bMag, dots, boundaries, interactionLocation,
           px, py, particleDirection, 'outgoing');
+
+        console.log('particle direction: ', particleDirection);
+        console.log('pathParams: ', pathParams);
+
         this.eventActivatedDots.push({
           dotIndices: pathParams.activatedDots,
           mass: particle.mass,
           index: activatedDotIndex,
-          name: particle.particle_name
+          name: particle.particle_name,
+          incoming: false,
+          CW: particleDirection === 'cw'
         });
         activatedDotIndex++;
       }
     });
+
+    console.log('activated dots: ', this.eventActivatedDots);
+
   }
 
   // this method determines which set of eventActivatedDots best matches
@@ -168,4 +209,49 @@ export class EventEnergyMomentumComponent implements OnInit {
     return bestIndex;
   }
 
+  /**
+   * this method checks for some gross errors, such as:
+   *  - the number of student-created circles is > or < than the # of charged particles
+   *  - two or more student-created circles appear to correspond to the same same charged particle
+   */
+  checkErrors() {
+    // check # of student circles and compare against event data
+    if (this.eventData.circles && this.eventData.circles.length > this.numberChargedParticles) {
+      this.errorMessages.push('The number of student-created circles exceeds the number of charged particles in the event.');
+      this.studentDataIsValid = false;
+    }
+    if (this.eventData.circles && this.eventData.circles.length < this.numberChargedParticles) {
+      this.errorMessages.push('The number of student-created circles is less than the number of charged particles in the event.');
+      this.studentDataIsValid = false;
+    }
+
+    // check "incoming" and "outgoing" properties for students' data
+    let circleNumber: number = 0;
+    this.studentData.forEach(circle => {
+      if (this.eventActivatedDots[circle.activatedDotsIndex].incoming !== circle.incoming) {
+        this.studentDataIsValid = false;
+        circle.error = true;
+        this.errorMessages.push(`Circle #${circleNumber+1}: incoming/outgoing property of the circle does not match that of the particle in the event.`);
+      }
+      //check for a repeat
+      for (var i = circleNumber+1; i < this.studentData.length; i++) {
+        console.log('circleNumber: ', circleNumber, 'i: ', i);
+        if (circle.activatedDotsIndex === this.studentData[i].activatedDotsIndex) {
+          // two circles refer to the same set of dots generated by a charged particle....
+          this.studentDataIsValid = false;
+          circle.error = true;
+          this.studentData[i].error = true;
+          this.errorMessages.push(`Circle #${circleNumber+1} and Circle #${i+1} appear to refer to the same charged particle track.`);
+        }
+      }
+      //check the sense of the rotation (CW or CCW) to see if the student's data matches the actual event
+      if (this.eventActivatedDots[circle.activatedDotsIndex].CW !== circle.CW) {
+        this.studentDataIsValid = false;
+        circle.error = true;
+        this.errorMessages.push(`Circle #${circleNumber+1}: the rotation (CW/CCW) property of the student-created circle does not match that of the particle in the event.`);
+      }
+      circleNumber++;
+    });
+  }
+  
 }
