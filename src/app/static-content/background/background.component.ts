8f0c67c99805b384cc68f5cd41db68d3b6e7352e
Hanbit  Lee <hanbitlee@hanbits-air.campus.tayloru.edu>
2018-07-24 14:44:45 -0400

    footnote change

--- src/app/static-content/background/background.component.ts
+++ src/app/static-content/background/background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import {NeutralDecaysComponent} from '../../shared/static-content/neutral-decays/neutral-decays.component';
 import {ChargedDecaysComponent} from '../../shared/static-content/charged-decays/charged-decays.component';
@@ -9,10 +9,38 @@ import {ChargedDecaysComponent} from '../../shared/static-content/charged-decays
   styleUrls: ['./background.component.css']
 })
 export class BackgroundComponent implements OnInit {
+  @ViewChild('footnotes') footnotesSection
+  @ViewChild('fn1Source') fn1Source
+  @ViewChild('fn2Source') fn2Source
+  @ViewChild('fn3Source') fn3Source
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  gotoFootnotesSection() {
+    this.footnotesSection.nativeElement.scrollIntoView();
+  }
+
+  goBackToText(fnNumber: string) {
+    switch(fnNumber) {
+      case 'fn1': {
+        this.fn1Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn2': {
+        this.fn2Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn3': {
+        this.fn3Source.nativeElement.scrollIntoView();
+        break;
+      }
+      default: {
+        //statements;
+        break;
+     }
+   }
+ }
 }
