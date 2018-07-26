fc6b8ad5e5b15e0b1b224ad788dec4bf7e6ee286
Hanbit  Lee <hanbitlee@hanbits-air.campus.tayloru.edu>
2018-07-26 14:18:35 -0400

    footnote/picture-changes

--- src/app/static-content/advanced-background/advanced-background.component.ts
+++ src/app/static-content/advanced-background/advanced-background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-advanced-background',
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-background.component.css']
 })
 export class AdvancedBackgroundComponent implements OnInit {
+  @ViewChild('footnotes') footnotesSection
+  @ViewChild('fn1Source') fn1Source
+  @ViewChild('fn2Source') fn2Source
+  @ViewChild('fn3Source') fn3Source
+  @ViewChild('fn4Source') fn4Source
+  @ViewChild('fn5Source') fn5Source
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  gotoFootnotesSection() {
+    this.footnotesSection.nativeElement.scrollIntoView();
+  }
+
+  goBackToText(fnNumber: string) {
+    switch(fnNumber) {
+      case 'fn1': {
+        this.fn1Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn2': {
+        this.fn2Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn3': {
+        this.fn3Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn4': {
+        this.fn4Source.nativeElement.scrollIntoView();
+        break;
+      }
+      case 'fn5': {
+        this.fn5Source.nativeElement.scrollIntoView();
+        break;
+      }
+      default: {
+        //statements;
+        break;
+     }
+   }
+ }
 }
--- src/app/static-content/demo/demo.component.spec.ts
+++ src/app/static-content/demo/demo.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {NeutralDecaysComponent} from '../../shared/static-content/neutral-decays/neutral-decays.component';
+import {ChargedDecaysComponent} from '../../shared/static-content/charged-decays/charged-decays.component';
 
 import { DemoComponent } from './demo.component';
 
