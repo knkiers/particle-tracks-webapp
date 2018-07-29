8766f48e513d3ff71a61575750ecb10a4a800ea2
Hanbit  Lee <hanbitlee@Hanbits-MacBook-Air.local>
2018-07-28 22:40:27 -0400

    Jul26

--- src/app/static-content/demo/demo.component.spec.ts
+++ src/app/static-content/demo/demo.component.spec.ts
@@ -1,6 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {NeutralDecaysComponent} from '../../shared/static-content/neutral-decays/neutral-decays.component';
-import {ChargedDecaysComponent} from '../../shared/static-content/charged-decays/charged-decays.component';
 
 import { DemoComponent } from './demo.component';
 
