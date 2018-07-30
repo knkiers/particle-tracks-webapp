4869eb49846626f5fefe46031a80f4ba7142d94a
Ken Kiers <knkiers@taylor.edu>
2018-07-30 19:23:51 -0400

    updates to profile page

--- src/app/authentication/profile/profile.component.ts
+++ src/app/authentication/profile/profile.component.ts
@@ -30,7 +30,7 @@ export class ProfileComponent implements OnInit {
 
   availableInstitutions: any = null;
   haveInstitutions: boolean = false;
-  
+
   currentUser: User = null;
   signinServerError: any;
 
@@ -93,6 +93,7 @@ export class ProfileComponent implements OnInit {
   }
 
 
+  // https://netbasal.com/disabling-form-controls-when-working-with-reactive-forms-in-angular-549dd7b42110
   enableField(fieldName: string) {
     switch(fieldName) {
       case 'username': {
